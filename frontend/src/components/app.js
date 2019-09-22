efa0c7512243b5b268f1c3efdceaa531255ffb43
Fue <fher.209@gmail.com>
2019-09-22 16:49:30 -0700

    Add navbar with links to signup, login, and logout

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
-// import NavBarContainer from './nav/navbar_container';
+import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
   <div>
-    {/* <NavBarContainer /> */}
+    <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
---
+++ frontend/src/components/nav/navbar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+// import './navbar.css'
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.getLinks = this.getLinks.bind(this);
+  }
+
+  logoutUser(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+  // Selectively render links dependent on whether the user is logged in
+  getLinks() {
+    if (this.props.loggedIn) {
+      return (
+        <div>
+          <button onClick={this.logoutUser}>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to={'/signup'}>Signup</Link>
+          <Link to={'/login'}>Login</Link>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Jumpy Kart</h1>
+        {this.getLinks()}
+      </div>
+    );
+  }
+}
+
+export default NavBar;
