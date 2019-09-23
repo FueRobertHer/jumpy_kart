e517b0b7f7f1a693c8714556c96f1b744c7a6121
Taehoon <songyi3279@gmail.com>
2019-09-23 10:55:02 -0700

    created Score Model and routes

---
+++ routes/api/scores.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Score = require('../../models/Score');
+
+router.get('/', (req,res) => {
+  Score.find()
+    .sort({value: 'desc'})
+    .then(scores => res.json(scores))
+    .catch(err => res.status(404).json({ noscoresfound: "No Scores found"}))
+});
+
+router.get('/user/:user_id', (req, res) => {
+  Score.find({user: req.params.user_id})
+    .then(scores => res.json(scores))
+    .catch(err => 
+      res.status(404).json({ noscoresfound: "No Scores found" })
+    )
+})
+
+router.post('/',
+
+  //passport makes sure that user key is present
+  passport.authenticate('jwt', { session: false}),
+  (req, res) => {  
+    const newScore = new Score({
+      username: req.body.username,
+      value: req.body.score,
+      date: Date.now()
+    })
+  }
+) 
