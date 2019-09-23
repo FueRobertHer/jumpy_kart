6429591271b29997a7c6d396e6ba855483c50c88
owenhaupt <owenshaupt@gmail.com>
2019-09-23 11:14:40 -0700

    f

---
+++ frontend/src/components/game/game.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // time left
+      // players
+      // races left
+      // gameStarted (from StarfighterPVP)
+    }
+  }
+
+  render() {
+    // const canv = document.getElementById('canv');
+    // const ctx
+
+    return (
+      <div>
+        <canvas
+          id="canv"
+        >
+
+        </canvas>
+
+
+      </div>
+    )
+  }
+}
+
+export default Canvas;
+
+// 5C93FC
