c9d8356df9cc8d4707dd56885c28bed3a95e2546
Andrew <drewhuangg@gmail.com>
2019-09-23 19:36:18 -0700

    created a dummy user component

--- app.js
+++ app.js
@@ -11,21 +11,19 @@ const path = require("path");
 
 import users from "./routes/api/users";
 
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
-app.use(passport.initialize());
-require('./config/passport').default(passport);
-
-app.use(urlencoded({ extended: false }));
-app.use(json());
-
-app.use("/api/users", users);
-
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 // app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+http.listen(PORT, () => {
+  console.log(`listening on localhost ${PORT}`)
+});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -38,14 +36,19 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(5000, () => {
-  console.log('listening on localhost 5000')
-});
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());  
+require('./config/passport').default(passport);
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+app.use("/api/users", users);
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
+
+
---
+++ frontend/src/components/game/user/user.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class User extends React.Component {
+
+  constructor(props){
+    super(props)
+
+  }
+
+
+
+}
+
+export default User;
