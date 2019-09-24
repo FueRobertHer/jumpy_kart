9e7fe2baa47eb567497bb6d4aa846f135bf426cb
owenhaupt <owenshaupt@gmail.com>
2019-09-24 16:23:23 -0700

    refactor draw methods

--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import marioSprite from '../../assets/images/mario_sprite.png';
-import pipeSprite from '../../assets/images/pipes_sprite.png';
+// import marioSprite from '../../assets/images/mario_sprite.png';
+// import pipeSprite from '../../assets/images/pipes_sprite.png';
 import { Pipe } from '../../classes/pipe';
+import * as DrawUtil from './drawUtil';
 
 class Canvas extends React.Component {
   constructor(props) {
@@ -13,34 +14,36 @@ class Canvas extends React.Component {
       // races left
       // gameStarted (from StarfighterPVP)
     }
+
+    this.drawObjects = this.drawObjects.bind(this);
   }
 
   componentDidMount() {
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#5C93FC";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    let mario = new Image();
-    mario.src = marioSprite;
-    mario.onload = () => {
-      ctx.drawImage(mario, 100, 100);
-    }
-    let pipe = new Image();
-    pipe.src = pipeSprite;
-    pipe.onload = () => {
-      ctx.drawImage(pipe, Pipe.width, 200);
-    }
+    this.drawBackground(ctx);
+    this.drawObjects(ctx);
     // simulating pulling value from backend to set y-coordinate of pipe
   }
 
-  drawObjects() {
+  drawBackground(ctx) {
+    ctx.fillStyle = "#5C93FC";
+    ctx.fillRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+  }
 
+  drawObjects(ctx) {
+    DrawUtil._drawKart(ctx)
+    DrawUtil._drawPipes(ctx)
   }
 
   render() {
+    if (!this.props) {
+      return null;
+    }
+
     return (
       <div>
-        <canvas ref="canvas" width="900" height="300"/>
+        <canvas ref="canvas" width="900" height="500"/>
       </div>
     )
   }
