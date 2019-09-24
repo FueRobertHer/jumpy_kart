37e7817c46548b756a851d00c2c88ef6f936fbfd
Andrew <drewhuangg@gmail.com>
2019-09-23 18:19:51 -0700

    websockets added to server side ...app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-import express from "express";
+import express from 'express';
 const mongoose = require('mongoose');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 import { mongoURI as db } from './config/keys';
 import { urlencoded, json } from 'body-parser';
 import passport from 'passport';
 const path = require("path");
 
 import users from "./routes/api/users";
-// import tweets from "./routes/api/tweets";
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -23,7 +25,25 @@ app.use(json());
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('sample', (example) => {
+    console.log('sample: ' + example);
+  });
+});
+
+http.listen(5000, () => {
+  console.log('listening on localhost 5000')
+});
+
+
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
-
 import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
-const App = () => (
-  <div>
-    <NavBarContainer />
-    <Switch>
-      <AuthRoute exact path="/" component={MainPage} />
-      <AuthRoute exact path="/login" component={LoginFormContainer} />
-      <AuthRoute exact path="/signup" component={SignupFormContainer} />
-    </Switch>
-  </div>
-);
+import 'whatwg-fetch';
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:5000');
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.sendSocketIO = this.sendSocketIO.bind(this);
+  }
+ 
+
+  sendSocketIO() {
+    socket.emit('example_message', 'demo');
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBarContainer />
+        <Switch>
+          <AuthRoute exact path="/" component={MainPage} />
+          <AuthRoute exact path="/login" component={LoginFormContainer} />
+          <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        </Switch>
+
+        <div>
+          <button onClick={this.sendSocketIO}></button>
+        </div>
+      </div>
+    )
+  }
+};
 
 export default App;
