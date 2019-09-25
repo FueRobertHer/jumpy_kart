2c831ef5a02a98377dc7b2291e8e6d92d8a991c1
Fue <fher.209@gmail.com>
2019-09-25 12:59:52 -0700

    Working with css animations

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,7 +5,7 @@ class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="main-container">
         <div className="main">
           <Link to="/login" ><h1 className="play">PLAY</h1></Link>
           {/* <h1>JUMPY KART</h1> */}
@@ -13,9 +13,8 @@ class MainPage extends React.Component {
         </div>
 
         <div>
-          <marquee>
-            hi
-            <div className="main-kart"></div>
+          <marquee className="marquee" direction="right" scrollamount="30">
+            <p className="main-kart"></p>
           </marquee>
         </div>
       </div>
