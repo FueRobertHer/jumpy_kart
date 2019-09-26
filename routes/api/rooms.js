60351f82d4090fb355ccf70b03452b7cdae4f627
Fue <fher.209@gmail.com>
2019-09-26 12:01:29 -0700

    Add rooms route and room util

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -1,1 +1,9 @@
-const express = require('express');
+import { Router } from "express";
+const router = Router();
+
+router.get('/'), (req, res) => {
+  let roomId = Math.random().toString(36).slice(3, 11); //returns a random 8 char string
+  res.json({roomId})
+}
+
+export default router;
