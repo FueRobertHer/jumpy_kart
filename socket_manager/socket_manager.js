11e598807942d2a07ea3b070477c26da2f4b31ac
owenhaupt <owenshaupt@gmail.com>
2019-09-27 17:20:06 -0700

    pre-pipes merge

--- socket_manager/socket_manager.js
+++ socket_manager/socket_manager.js
@@ -2,9 +2,9 @@ import { io } from '../app';
 import Game from '../src/classes/game';
 
 
-//Have the server side store the game ‘state’, be able to modify the state, and also receive updates from the players
+//Have the server side store the game 'state', be able to modify the state, and also receive updates from the players
 
-// Have the client side be able to receive and render the ‘game’ state sent by the server and also send out player actions to the server
+// Have the client side be able to receive and render the 'game' state sent by the server and also send out player actions to the server
 
 export const gameState = {
   users: {},
@@ -14,7 +14,7 @@ export const gameState = {
 
 
 export const socketManager = (socket) => {
-  
+
   console.log('a user connected');
   //test case - on connection, render game
   let game;
@@ -32,22 +32,22 @@ export const socketManager = (socket) => {
       });
       gameState.users[socket.id] = game.addPlayer(roomInfo.userId, socket);
       player = gameState.users[roomInfo.userId];
-    } 
+    }
 
-    // if (roomInfo.type === "joinRoom") {
-    //   if (gameState.rooms[roomInfo.roomId].gameId === roomInfo.roomId) {
-    //     socket.id = gameState.rooms[roomInfo.roomId].gameId;
-    //     game = gameState.rooms[roomInfo.roomId];
-    //     socket.on('loadGame', () => {
-    //       console.log('loading game');
-    //       game.loadGame(socket);
-    //     });
-    //     gameState.users[roomInfo.userId] = game.addPlayer(roomInfo.userId, socket);
-    //     player = gameState.users[roomInfo.userId];
-    //   } else {
-    //     return null;
-    //   }
-    // }
+    if (roomInfo.type === "joinRoom") {
+      if (gameState.rooms[roomInfo.roomId].gameId === roomInfo.roomId) {
+        socket.id = gameState.rooms[roomInfo.roomId].gameId;
+        game = gameState.rooms[roomInfo.roomId];
+        socket.on('loadGame', () => {
+          console.log('loading game');
+          game.loadGame(socket);
+        });
+        gameState.users[roomInfo.userId] = game.addPlayer(roomInfo.userId, socket);
+        player = gameState.users[roomInfo.userId];
+      } else {
+        return null;
+      }
+    }
 
   });
 
@@ -55,7 +55,7 @@ export const socketManager = (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  
+
 };
 
-export default socketManager;
+export default socketManager; 
