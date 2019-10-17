3cb5446b6ea76e1c03ff2c78b7e370a6e36a850a
owenshaupt <owenshaupt@gmail.com>
2019-10-17 14:18:42 -0700

    strated adding lobby redirect button

--- frontend/src/components/podium/podium.jsx
+++ frontend/src/components/podium/podium.jsx
@@ -17,21 +17,28 @@ class Podium extends React.Component {
     this.firstPlace = this.podium[0] || "";
     this.secondPlace = this.podium[1] || "";
     this.thirdPlace = this.podium[2] || "";
+
+    this.backToLobby = this.backToLobby.bind(this);
   }
 
   componentDidMount() {
     this.podium.forEach(player => {
-      updateCoins(player)
+      updateCoins(player);
     });
 
     this.props.fetchHighScore();
+  }
+
+  backToLobby() {
+    location.reload();
+    this.props.history.push('/lobby');
   }
 
   render() {
     if (!this.firstPlace) return null;
 
     return (
-      <div>
+      <div className='podium-master'>
         <div className='background-div'>
           <PodiumModal />
           <div id='first-place' className='player-div'>
@@ -52,6 +59,11 @@ class Podium extends React.Component {
           >
             <p className='high-scores-button-text'>View High Scores!</p>
           </button>
+        </div>
+        <div>
+          <button className='back-to-lobby-button'>
+            <p className='high-scores-button-text'>Back to Lobby!</p>            
+          </button> />
         </div>
       </div>
     );
