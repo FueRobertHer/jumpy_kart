38ca866af27668cffb5440ebc1aeb6c50de9751f
owenshaupt <owenshaupt@gmail.com>
2020-02-19 15:34:25 -0800

    commented out console logs in game.jsx

--- frontend/src/components/game/game.jsx
+++ frontend/src/components/game/game.jsx
@@ -14,14 +14,14 @@ let conn_options = {
 };
 
 if (process.env.NODE_ENV !== "production") {
-  console.log(`process.env: ${process.env}`);
+  // console.log(`process.env: ${process.env}`);
   SERVER = io("http://localhost:5000", conn_options, {
     transports: ["websocket"]
   });
 }
 
 if (process.env.NODE_ENV === "production") {
-  console.log(`process.env: ${process.env}`);
+  // console.log(`process.env: ${process.env}`);
   SERVER = io();
 }
 
@@ -60,7 +60,7 @@ class Canvas extends React.Component {
   }
 
   openSocket() {
-    console.log("opening socket");
+    // console.log("opening socket");
     return new Promise(resolve => {
       this.socket = SERVER;
       let socket = this.socket;
